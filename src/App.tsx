aa24358777f027a9783e0ab820408742c33e52b2
Guilherme <guilherme.gl1997@gmail.com>
2020-08-11 12:32:02 -0300

    feat: routes creation

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      
+      <Routes>]
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
+      </Routes>
+
+      {/* <Footer /> */}
+
+      <GlobalStyles />
+    </BrowserRouter>
+  );
+}
+
+export default App;
