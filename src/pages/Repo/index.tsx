2a1be15ef8f447aeaa3c35da066cc29432cb2481
Guilherme <guilherme.gl1997@gmail.com>
2020-08-15 17:04:26 -0300

    feat: repo page

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { 
+  Container, 
+  Breadcrumb, 
+  RepoIcon, 
+  Stats, 
+  StarIcon, 
+  ForksIcon, 
+  LinkButton, 
+  GithubIcon, 
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={'username'} to={'/gui-leandro'}>
+          gui-leandro
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/gui-leandro/clone-github'}>
+          clone-github
+        </Link>
+      </Breadcrumb>
+
+      <p>Contain my github interface clone.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForksIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/gui-leandro/clone-github'}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
